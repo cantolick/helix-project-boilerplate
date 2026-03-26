9249f88295907a42961bbb451e74cd04ed17ac6f
Craig Antolick <cantolick@adobe.com>
2026-03-26 11:23:42 -0500

    update for universal editor

--- blocks/about/about.js
+++
@@ -1,9 +0,0 @@
-export default function enhanceContent(block) {
-  if (!block) return;
-
-  // Example: Add classes or attributes to specific elements
-  const headings = block.querySelectorAll('h2, h3, h4');
-  headings.forEach((heading) => {
-    heading.classList.add('main-heading');
-  });
-}
